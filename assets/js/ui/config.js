a0e0ec3ffe46ddb5b92f9eeb512a5e39ccaeccb6
Jennifer Nurse <jennifer@mobify.com>
2015-01-22 17:14:15 -0800

    Remove generated files from project.

--- assets/js/ui/config.js
+++
@@ -1,7 +0,0 @@
-require.config({
-    'paths': {
-        'fastclick': '../../../bower_components/fastclick/lib/fastclick',
-        'deckard': '../../../bower_components/deckard/dist/deckard.min',
-        'pre-tag': '../../../bower_components/pre-tag/index'
-    }
-});
